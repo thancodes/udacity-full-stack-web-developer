238b762b930deece45f7c96641ba2be2e11146b5
Tawatchai Rakpuen <me@tawatchairakpue.com>
2017-03-12 11:02:49 +0700

    fix: pg_config.sh file, add "pip install sqlalchemy".

--- item-catalog/vagrant/pg_config.sh
+++ item-catalog/vagrant/pg_config.sh
@@ -10,6 +10,7 @@ pip install redis
 pip install passlib
 pip install itsdangerous
 pip install flask-httpauth
+pip install sqlalchemy
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
@@ -17,9 +18,3 @@ su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
-
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-make install
